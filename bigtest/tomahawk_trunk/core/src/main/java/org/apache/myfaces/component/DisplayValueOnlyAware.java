de0e4ae52f5a6add8212bc0379756bf49bf49bae
Leonardo Uribe <lu4242@apache.org>
2008-05-08 00:30:28 +0000

    component generation part 1

---
+++ bigtest/tomahawk_trunk/core/src/main/java/org/apache/myfaces/component/DisplayValueOnlyAware.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.component;
+
+/**
+ * Behavioral interface.
+ * By default, displayValueOnly is false, and the components have the default behaviour.
+ * When displayValueOnly is true, the renderer should not render any input widget.
+ * Only the text corresponding to the component's value should be rendered instead.
+ * 
+ * org.apache.myfaces.shared_tomahawk.component.DisplayValueOnlyCapable 
+ * are available on shared, but we need something on tomahawk
+ * to define and generate properties. 
+ *
+ */
+public interface DisplayValueOnlyAware
+{
+    /**
+     * @JSFProperty
+     */
+    public Boolean getDisplayValueOnly();
+
+    public void setDisplayValueOnly(Boolean b);
+    
+    /**
+     * @JSFProperty
+     */
+    public String getDisplayValueOnlyStyle();
+    
+    public void setDisplayValueOnlyStyle(String displayValueOnlyStyle);
+    
+    /**
+     * @JSFProperty
+     */
+    public String getDisplayValueOnlyStyleClass();
+    
+    public void setDisplayValueOnlyStyleClass(String displayValueOnlyStyleClass);    
+    
+}
