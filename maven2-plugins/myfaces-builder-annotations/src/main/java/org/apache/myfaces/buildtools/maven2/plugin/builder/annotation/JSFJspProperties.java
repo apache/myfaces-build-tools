ae184a204494c52ff4ee87d0d210cd1d9fee087e
Leonardo Uribe <lu4242@apache.org>
2008-06-06 03:01:25 +0000

    add RetentionType source to this tag (cause problems when compile)

--- maven2-plugins/myfaces-builder-annotations/src/main/java/org/apache/myfaces/buildtools/maven2/plugin/builder/annotation/JSFJspProperties.java
+++ maven2-plugins/myfaces-builder-annotations/src/main/java/org/apache/myfaces/buildtools/maven2/plugin/builder/annotation/JSFJspProperties.java
@@ -18,6 +18,15 @@
  */
 package org.apache.myfaces.buildtools.maven2.plugin.builder.annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
 public @interface JSFJspProperties
 {
 
