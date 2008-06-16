23f84b26d4dd64aace66e684b154ba741bee7771
Simon Kitching <skitching@apache.org>
2008-06-16 20:56:37 +0000

    Add documentation. Reorder methods. No logic changes.

--- maven2-plugins/myfaces-builder-plugin/src/main/java/org/apache/myfaces/buildtools/maven2/plugin/builder/model/ClassMeta.java
+++ maven2-plugins/myfaces-builder-plugin/src/main/java/org/apache/myfaces/buildtools/maven2/plugin/builder/model/ClassMeta.java
@@ -29,30 +29,51 @@ import org.apache.myfaces.buildtools.maven2.plugin.builder.io.XmlWriter;
 /**
  * Base class for various metadata (model) classes.
  * <p>
- * This holds metadata common to all model classes that represent a jsf class,
- * eg a component, converter, validator. In all these cases, the class has a
+ * This holds metadata common to all model classes that represent a JSF entity,
+ * eg a component, converter, validator, renderer. In all these cases, the class has a
  * type, a parent and optionally a list of implemented interfaces.
+ * <p>
+ * Instances of this type are intended primarily to map to entries in generated
+ * JSP TLD files, faces-config.xml files and similar. The "class" attribute on
+ * this class contains the name of the java class that implements that entity.
+ * <p>
+ * However there are also instances of this class that represent "abstract"
+ * entities that are not directly referenced by TLD or faces-config files; they
+ * exist to represent ancestor classes of the concrete entities. 
+ * <p>
+ * The instances are created via annotations on Java classes. Therefore each
+ * ClassMeta has a "sourceClass" property that records the name of the annotated
+ * class. In simple cases, "class" and "sourceClass" are the same, ie the JSF entity
+ * <i>is</i> the class that holds the relevant annotations. But this is not true in
+ * the case of "code generation"; here the class property references the actual JSF
+ * entity (the generated class).
+ * <p>
+ * As described in the documentation for the parent attribute, the ClassMeta objects
+ * describe a java inheritance tree, but it is not quite the same as the real java
+ * inheritance hierarchy for the annotated java classes. The ClassMeta data omits
+ * any classes in the hierarchy that are not annotated, as they are not relevant
+ * for the purposes of metadata inheritance.
  */
 public class ClassMeta
 {
     private String _className;
     private String _parentClassName;
-    private String _superClassName;
     
     private List _interfaceClassNames = new ArrayList();
     private String _modelId;
     private String _classSource;
+    private String _superClassName;
 
     /**
      * Write this model out as xml.
      */
     public static void writeXml(XmlWriter out, ClassMeta mi)
     {
+        out.writeElement("modelId", mi._modelId);
         out.writeElement("className", mi._className);
         out.writeElement("parentClassName", mi._parentClassName);
-        out.writeElement("superClassName", mi._superClassName);
-        out.writeElement("modelId", mi._modelId);
         out.writeElement("classSource", mi._classSource);
+        out.writeElement("superClassName", mi._superClassName);
 
         if (!mi._interfaceClassNames.isEmpty())
         {
@@ -74,19 +95,41 @@ public class ClassMeta
      */
     public static void addXmlRules(Digester digester, String prefix)
     {
+        digester.addBeanPropertySetter(prefix + "/modelId");
         digester.addBeanPropertySetter(prefix + "/className");
         digester.addBeanPropertySetter(prefix + "/parentClassName");
-        digester.addBeanPropertySetter(prefix + "/superClassName");
         digester.addBeanPropertySetter(prefix + "/classSource");
-        digester.addBeanPropertySetter(prefix + "/modelId");
+        digester.addBeanPropertySetter(prefix + "/superClassName");
         digester.addCallMethod(prefix + "/interfaces/interface",
                 "addInterfaceClassName", 1);
         digester.addCallParam(prefix + "/interfaces/interface", 0, "name");
     }
 
     /**
-     * The fully-qualified name of the class that this metadata was extracted
-     * from.
+     * Indicates which "group" of metadata this class belongs to.
+     * <p>
+     * Projects can inherit metadata from other projects, in which case
+     * all the ClassMeta objects end up in one big collection. But for
+     * some purposes it is necessary to iterate over the objects belonging
+     * to only one project (eg when generating components). This return
+     * value can be tested to check which "group" (project) a particular
+     * instance belongs to.
+     */
+    public String getModelId()
+    {
+        return _modelId;
+    }
+
+    public void setModelId(String _modelId)
+    {
+        this._modelId = _modelId;
+    }
+
+    /**
+     * The fully-qualified name of the JSF entity class, ie the class that actually
+     * implements a Component, Converter, Validator, Renderer, etc.
+     * <p>
+     * The specified class may be a hand-written one, or one created via code generation. 
      */
     public String getClassName()
     {
@@ -99,12 +142,28 @@ public class ClassMeta
     }
 
     /**
-     * The nearest relevant ancestor class of the class that this metadata was
+     * Utility method to return just the packagename part of the className
+     * attribute.
+     */
+    public String getPackageName()
+    {
+        return StringUtils.substring(getClassName(), 0, StringUtils.lastIndexOf(getClassName(), '.'));
+    }
+
+    /**
+     * The nearest relevant (annotated) ancestor class of the class that this metadata was
      * extracted from.
      * <p>
      * For example, when a class is marked as a Component class, then this will
      * refer to the nearest ancestor class that is also marked as a Component
-     * class.
+     * class. Note that this is "consistent with" the actual Java class hierarchy for 
+     * annotated classes that were used to create this data, but skips java classes
+     * that were not annotated (and are therefore irrelevant for metadata purposes).
+     * <p>
+     * The value of this attribute will match the className attribute of another ClassMeta
+     * object. Following the chain of ParentClassName links (plus Interface links) gives all
+     * the ClassMeta objects needed to determine the set of metadata properties for a JSF
+     * entity.    
      */
     public String getParentClassName()
     {
@@ -114,35 +173,6 @@ public class ClassMeta
     public void setParentClassName(String className)
     {
         _parentClassName = className;
-    }
-
-    /**
-     * The superClassName is the fully qualified name of
-     * the class that should be parent of the class representing
-     * this component. The difference between this and 
-     * the parentClassName is that the parent reference
-     * the class that is a full component which is inside the
-     * hierarchy and extends the properties. One useful example 
-     * about why this semantic is important is use the abstract
-     * pattern to generate classes. The abstract class is not
-     * a full component (never instantiated, just a holder of
-     * code) but usually parent class of the abstract
-     * is a full component.
-     * 
-     */
-    public void setSuperClassName(String superClassName)
-    {
-        this._superClassName = superClassName;
-    }
-
-    public String getSuperClassName()
-    {
-        if (_superClassName == null){
-            //return the parent class name instead.
-            return getParentClassName();
-        }else{
-            return _superClassName;            
-        }        
     }
 
     /**
@@ -167,19 +197,12 @@ public class ClassMeta
         _interfaceClassNames.add(name);
     }
 
-    public void setModelId(String _modelId)
+    /**
+     * Return the className of the real java class from which this metadata was gathered.
+     */
+    public String getClassSource()
     {
-        this._modelId = _modelId;
-    }
-
-    public String getModelId()
-    {
-        return _modelId;
-    }
-
-    public String getPackageName()
-    {
-        return StringUtils.substring(getClassName(), 0, StringUtils.lastIndexOf(getClassName(), '.'));
+        return _classSource;
     }
 
     public void setClassSource(String classSource)
@@ -187,8 +210,32 @@ public class ClassMeta
         this._classSource = classSource;
     }
 
-    public String getClassSource()
+    /**
+     * Return the real java parent class of the class from which this metadata
+     * was gathered (see classSource property).
+     * <p>
+     * This value is usually the same as property parentClassName. However in
+     * the case where the direct parent of this class is not annotated then
+     * parentClassName can point to some more remote ancestor (ie some
+     * ancestor of superClassName).
+     * <p>
+     * This information is needed for code generation.
+     */
+    public void setSuperClassName(String superClassName)
     {
-        return _classSource;
+        this._superClassName = superClassName;
+    }
+
+    public String getSuperClassName()
+    {
+        if (_superClassName == null)
+        {
+            //return the parent class name instead.
+            return getParentClassName();
+        }
+        else
+        {
+            return _superClassName;            
+        }        
     }
 }
