6bd901a99521e1a5bc551286305e91c08d75f9bc
Simon Kitching <skitching@apache.org>
2008-06-21 13:00:57 +0000

    Change sort order for elements. Needed when comparing files from two different implementations, as the <class> tags vary.

--- other/normalizer/src/main/java/org/apache/myfaces/buildtools/normalizer/Element.java
+++ other/normalizer/src/main/java/org/apache/myfaces/buildtools/normalizer/Element.java
@@ -13,10 +13,31 @@ import java.util.List;
  */
 public class Element implements Comparable
 {
+    static String[] SORT_PRIORITY_HIGH = new String[]
+    {
+        "name",
+    };
+    
+    static String[] SORT_PRIORITY_LOW = new String[]
+    {
+        "class"
+    };
+
     String name;
     Attribute[] attributes;
     String content;
     List children = new LinkedList();
+    
+    // Value used to choose ordering for two elements with different names.
+    // Elements whose name matches a "high priority" pattern are output before
+    // elements with "normal" names, which are output before elements whose
+    // names match a "low priority" pattern.
+    int sortPriority = 0;
+
+    // Value used to choose ordering for two elements that have the same
+    // name and no text content. This is built from the text content of
+    // the (ordered) set of child elements, so that two nodes with similar
+    // child elements get sorted in the same order.
     String sortKey = null;
 
     // The attributes array is expected to be sorted before passing in here.
@@ -24,6 +45,7 @@ public class Element implements Comparable
     {
         this.name = name;
         this.attributes = attributes;
+        sortPriority = getSortPriority(name);
     }
 
     public void setContent(String content)
@@ -56,8 +78,11 @@ public class Element implements Comparable
 
         // now sort the list of children relative to each other
         Collections.sort(children);
-        
-        // and compute the sortKey for this element
+
+        // Compute the sortKey for this element, used when
+        // two elements have identical names and therefore need
+        // to be ordered according to the content of their
+        // child elements.
         StringBuffer buf = new StringBuffer();
         for(Iterator i = children.iterator(); i.hasNext(); )
         {
@@ -127,16 +152,55 @@ public class Element implements Comparable
             return -1;
         else if (!this.children.isEmpty() && otherElement.children.isEmpty())
             return +1;
-        
-        // Sort tags with different names alphabetically
-        int compare = name.compareTo(otherElement.name);
-        if (compare != 0)
+
+        if (name.equals(otherElement.name))
         {
-            return compare;
+            // Ok, we have two tags with the same name and both have child elements.
+            // Sort by the sortkey we computed during the sortChildren method
+            return sortKey.compareTo(otherElement.sortKey);
         }
 
-        // Ok, we have two tags with the same name and both have child elements.
-        // Sort by the sortkey we computed during the sortChildren method
-        return sortKey.compareTo(otherElement.sortKey);
+        // Names are different: check whether either name has special priority.
+        if (sortPriority != otherElement.sortPriority)
+        {
+            return otherElement.sortPriority - sortPriority;
+        }
+
+        // Ok, different names and the priority of the name is equal.
+        // So just sort alphabetically.
+        return name.compareTo(otherElement.name);
+    }
+
+    /**
+     * Return the "priority" of this name.
+     * <p>
+     * Names with higher priority are sorted earlier in the list of elements.
+     * And as elements with equal names are sorted according to the contents
+     * of their children, this also affects the sorting order of the parent
+     * element of an element that has a "priority" value.
+     * <p>
+     * Normal element names have zero priority.
+     */
+    private int getSortPriority(String ename)
+    {
+        for(int i=0; i<SORT_PRIORITY_HIGH.length; ++i)
+        {
+            if (ename.contains(SORT_PRIORITY_HIGH[i]))
+            {
+                // always positive
+                return SORT_PRIORITY_HIGH.length - i;
+            }
+        }
+
+        for(int i=0; i<SORT_PRIORITY_LOW.length; ++i)
+        {
+            if (ename.contains(SORT_PRIORITY_LOW[i]))
+            {
+                // always negative
+                return i - SORT_PRIORITY_LOW.length;
+            }
+        }
+        
+        return 0;
     }
 }
