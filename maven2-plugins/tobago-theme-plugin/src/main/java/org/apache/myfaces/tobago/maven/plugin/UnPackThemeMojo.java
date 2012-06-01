0eeb200d9ca8d46dccb4d3470799f25dc6cdfa9c
Bernd Bohmann <bommel@apache.org>
2012-06-01 19:45:36 +0000

    (TOBAGO-945) Add the version of Tobago to the resources URL to avoid caching when updating Tobago

--- maven2-plugins/tobago-theme-plugin/src/main/java/org/apache/myfaces/tobago/maven/plugin/UnPackThemeMojo.java
+++ maven2-plugins/tobago-theme-plugin/src/main/java/org/apache/myfaces/tobago/maven/plugin/UnPackThemeMojo.java
@@ -23,15 +23,24 @@ import org.codehaus.plexus.archiver.ArchiverException;
 import org.codehaus.plexus.archiver.UnArchiver;
 import org.codehaus.plexus.archiver.manager.ArchiverManager;
 import org.codehaus.plexus.archiver.manager.NoSuchArchiverException;
+import org.codehaus.plexus.archiver.zip.ZipEntry;
+import org.codehaus.plexus.archiver.zip.ZipFile;
 import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.IOUtil;
+import org.codehaus.plexus.util.ReaderFactory;
+import org.codehaus.plexus.util.xml.XmlStreamReader;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.codehaus.plexus.util.xml.Xpp3DomBuilder;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.FileInputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.Locale;
-import java.util.zip.ZipInputStream;
-import java.util.zip.ZipEntry;
+import java.util.Properties;
 
 /**
  *
@@ -65,19 +74,35 @@ public class UnPackThemeMojo extends AbstractThemeMojo {
    */
   private File webappDirectory;
 
+  /**
+   * Don't use the versioned information of the themes..
+   *
+   * @parameter
+   */
+  private boolean ignoreVersioned;
 
-  private boolean findThemeDescriptor(File jarFile) throws MojoExecutionException {
-    ZipInputStream zip = null;
+
+  private String getThemeDescriptor(File jarFile) throws MojoExecutionException {
+    ZipFile zip = null;
     try {
-      zip = new ZipInputStream(new FileInputStream(jarFile));
-      while (zip.available() > 0) {
-        ZipEntry nextEntry = zip.getNextEntry();
+      zip = new ZipFile(jarFile);
+      Enumeration files = zip.getEntries();
+      while (files.hasMoreElements()) {
+        ZipEntry nextEntry = (ZipEntry) files.nextElement();
         if (nextEntry == null || nextEntry.isDirectory()) {
           continue;
         }
         String name = nextEntry.getName();
-        if (name.equals("META-INF/tobago-theme.xml")|| name.equals("META-INF/tobago-config.xml")) {
-          return true;
+        if (name.equals("META-INF/tobago-theme.xml") || name.equals("META-INF/tobago-config.xml")) {
+          XmlStreamReader xsr = null;
+          try {
+            StringWriter stringWriter = new StringWriter();
+            xsr = ReaderFactory.newXmlReader(zip.getInputStream(nextEntry));
+            IOUtil.copy(xsr, stringWriter);
+            return stringWriter.toString();
+          } finally {
+            IOUtil.close(xsr);
+          }
         }
       }
     } catch (IOException e) {
@@ -91,8 +116,7 @@ public class UnPackThemeMojo extends AbstractThemeMojo {
         }
       }
     }
-
-    return false;
+    return null;
   }
 
   public void execute() throws MojoExecutionException {
@@ -104,37 +128,79 @@ public class UnPackThemeMojo extends AbstractThemeMojo {
       while (artifacts.hasNext()) {
 
         String artifact = (String) artifacts.next();
-        getLog().debug("Testing jar "+ artifact);
+        if (getLog().isDebugEnabled()) {
+          getLog().debug("Testing jar "+ artifact);
+        }
 
         File file = new File(artifact);
-        if (file.isFile() && artifact.endsWith(".jar") && findThemeDescriptor(file)) {
-          String name = file.getName();
-          File tempLocation = new File(workDirectory, name.substring(0, name.length() - 4));
-          boolean process = false;
-          if (!tempLocation.exists()) {
-            tempLocation.mkdirs();
-            process = true;
-          } else if (file.lastModified() > tempLocation.lastModified()) {
-            process = true;
-          }
-          if (process) {
-            getLog().debug("Expanding theme "+ name);
-            try {
-              unpack(file, tempLocation);
-              String[] fileNames = getThemeFiles(tempLocation);
-              for (int i = 0, fileNamesLength = fileNames.length; i < fileNamesLength; i++) {
-                String fileName = fileNames[i];
+        if (file.isFile() && artifact.endsWith(".jar")) {
+          String descriptor = getThemeDescriptor(file);
+          if (descriptor != null) {
 
-                File fromFile = new File(tempLocation, fileName);
-                File toFile = new File(webappDirectory, fileName);
+            String name = file.getName();
+            File tempLocation = new File(workDirectory, name.substring(0, name.length() - 4));
+            boolean process = false;
+            if (!tempLocation.exists()) {
+              tempLocation.mkdirs();
+              process = true;
+            } else if (file.lastModified() > tempLocation.lastModified()) {
+              process = true;
+            }
+            if (process) {
+              try {
+                unpack(file, tempLocation);
+                String version = null;
+                String resourcePath = null;
                 try {
-                  FileUtils.copyFile(fromFile, toFile);
+                  Xpp3Dom xpp3Dom = Xpp3DomBuilder.build(new StringReader(descriptor));
+                  Xpp3Dom themeDefinitions = xpp3Dom.getChild("theme-definitions");
+                  if (themeDefinitions != null && !ignoreVersioned) {
+                    for (Xpp3Dom themeDefinition : themeDefinitions.getChildren()) {
+                      Xpp3Dom versionedDom = themeDefinition.getChild("versioned");
+                      if (versionedDom != null) {
+                        boolean versioned = Boolean.parseBoolean(versionedDom.getValue());
+                        if (versioned) {
+                          Xpp3Dom resourcePathDom = themeDefinition.getChild("resource-path");
+                          resourcePath = resourcePathDom.getValue();
+                          Properties properties = new Properties();
+                          String metaInf = tempLocation + "/META-INF/MANIFEST.MF";
+                          properties.load(new StringReader(FileUtils.fileRead(metaInf)));
+                          version = properties.getProperty("Implementation-Version");
+                        }
+                      }
+                    }
+                  }
                 } catch (IOException e) {
-                  throw new MojoExecutionException("Error copy file: " + fromFile + "to: " + toFile, e);
+                  getLog().error(e);
+                } catch (XmlPullParserException e) {
+                  getLog().error(e);
                 }
+                if (getLog().isDebugEnabled()) {
+                  getLog().debug("Expanding theme: "+ name);
+                  getLog().debug("Version: " + version);
+                  getLog().debug("ResourcePath: " + resourcePath);
+                }
+                String[] fileNames = getThemeFiles(tempLocation);
+                for (int i = 0, fileNamesLength = fileNames.length; i < fileNamesLength; i++) {
+                  String fileName = fileNames[i];
+                  File fromFile = new File(tempLocation, fileName);
+                  String toFileName = fileName;
+                  if (resourcePath != null && toFileName.startsWith(resourcePath)) {
+                    toFileName = resourcePath + "/" + version + "/" +toFileName.substring(resourcePath.length()+1);
+                  }
+                  if (getLog().isDebugEnabled()) {
+                    getLog().debug("Copy file " + fromFile + " to: " + toFileName);
+                  }
+                  File toFile = new File(webappDirectory, toFileName);
+                  try {
+                    FileUtils.copyFile(fromFile, toFile);
+                  } catch (IOException e) {
+                    throw new MojoExecutionException("Error copy file: " + fromFile + " to: " + toFile, e);
+                  }
+                }
+              } catch (NoSuchArchiverException e) {
+                getLog().info("Skip unpacking dependency file with unknown extension: " + file.getPath());
               }
-            } catch (NoSuchArchiverException e) {
-              getLog().info("Skip unpacking dependency file with unknown extension: " + file.getPath());
             }
           }
         }
@@ -152,8 +218,6 @@ public class UnPackThemeMojo extends AbstractThemeMojo {
       unArchiver.setSourceFile(file);
       unArchiver.setDestDirectory(location);
       unArchiver.extract();
-    } catch (IOException e) {
-      throw new MojoExecutionException("Error unpacking file: " + file + "to: " + location, e);
     } catch (ArchiverException e) {
       throw new MojoExecutionException("Error unpacking file: " + file + "to: " + location, e);
     }
